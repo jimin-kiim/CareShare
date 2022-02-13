79370c6c0307ecfa99a2d8d2b45baee8e12c7e9c
Jimin Kim <0305jimin@gmail.com>
2022-02-13 14:33:35 +0900

    fix: merge errors

--- Home/PostDetail.js
+++ Home/PostDetail.js
@@ -4,7 +4,14 @@ import { StackScreenProps } from "@react-navigation/stack";
 
 import React, { useEffect, useState } from "react";
 
-export default function Post({ id, title, content, address, type, price }) {
+export default function PostDetail({
+    id,
+    title,
+    content,
+    address,
+    type,
+    price,
+}) {
     return (
         <View style={styles.post} key={id}>
             <Image
@@ -27,68 +34,57 @@ export default function Post({ id, title, content, address, type, price }) {
                 </View>
             </View>
         </View>
-        <Text style={styles.postType}>{type}</Text>
-        <View style={styles.postLastInfo}>
-          <Text style={styles.postPrice}>{price}원</Text>
-          <Image
-            source={require("../assets/ios-heart-empty.svg")}
-            style={styles.postHeart}
-          />
-        </View>
-      </View>
-    </View>
-  );
-};
+    );
+}
 
 const styles = StyleSheet.create({
-  post: {
-    flexDirection: "row",
-    paddingHorizontal: 25,
-    paddingTop: 25,
-  },
-  postContent: {
-    paddingLeft: 20,
-  },
-  postImg: {
-    width: 120,
-    height: 120,
-  },
-  postTitle: {
-    fontSize: 20,
-    color: theme.textDark,
-  },
-  postInfo: {
-    flexDirection: "row",
-    paddingTop: 5,
-    paddingBottom: 17,
-  },
-  postInfoText: {
-    fontSize: 15,
-    color: theme.textLight,
-  },
-  postType: {
-    fontSize: 15,
-    color: theme.textDark,
-    borderRadius: 15,
-    backgroundColor: "#EEEEEE",
-    paddingVertical: 3,
-    paddingHorizontal: 11,
-    alignSelf: "flex-start",
-  },
-  postLastInfo: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-  },
-  postPrice: {
-    fontSize: 18,
-    color: theme.textDark,
-    fontWeight: "700",
-    paddingTop: 6,
-  },
-  postHeart: {
-    width: 20,
-    height: 18,
-  },
+    post: {
+        flexDirection: "row",
+        paddingHorizontal: 25,
+        paddingTop: 25,
+    },
+    postContent: {
+        paddingLeft: 20,
+    },
+    postImg: {
+        width: 120,
+        height: 120,
+    },
+    postTitle: {
+        fontSize: 20,
+        color: theme.textDark,
+    },
+    postInfo: {
+        flexDirection: "row",
+        paddingTop: 5,
+        paddingBottom: 17,
+    },
+    postInfoText: {
+        fontSize: 15,
+        color: theme.textLight,
+    },
+    postType: {
+        fontSize: 15,
+        color: theme.textDark,
+        borderRadius: 15,
+        backgroundColor: "#EEEEEE",
+        paddingVertical: 3,
+        paddingHorizontal: 11,
+        alignSelf: "flex-start",
+    },
+    postLastInfo: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+    },
+    postPrice: {
+        fontSize: 18,
+        color: theme.textDark,
+        fontWeight: "700",
+        paddingTop: 6,
+    },
+    postHeart: {
+        width: 20,
+        height: 18,
+    },
 });
-export default PostDetail;
--- Home/PostForm.js
+++ Home/PostForm.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import Firebase from "../config/firebase";
+import { theme } from "../colors";
+
 import {
     StyleSheet,
     Text,
@@ -10,6 +12,7 @@ import {
     Dimensions,
     ImageBackground,
     TextInput,
+    Button,
 } from "react-native";
 export default function PostForm({ navigation }) {
     const [text, setText] = useState("");
@@ -69,67 +72,57 @@ export default function PostForm({ navigation }) {
             ></TextInput>
             <Button onPress={() => savePost}></Button>
         </View>
-        <Text style={styles.postType}>{type}</Text>
-        <View style={styles.postLastInfo}>
-          <Text style={styles.postPrice}>{price}원</Text>
-          <Image
-            source={require("../assets/ios-heart-empty.svg")}
-            style={styles.postHeart}
-          />
-        </View>
-      </View>
-    </View>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
-  post: {
-    flexDirection: "row",
-    paddingHorizontal: 25,
-    paddingTop: 25,
-  },
-  postContent: {
-    paddingLeft: 20,
-  },
-  postImg: {
-    width: 120,
-    height: 120,
-  },
-  postTitle: {
-    fontSize: 20,
-    color: theme.textDark,
-  },
-  postInfo: {
-    flexDirection: "row",
-    paddingTop: 5,
-    paddingBottom: 17,
-  },
-  postInfoText: {
-    fontSize: 15,
-    color: theme.textLight,
-  },
-  postType: {
-    fontSize: 15,
-    color: theme.textDark,
-    borderRadius: 15,
-    backgroundColor: "#EEEEEE",
-    paddingVertical: 3,
-    paddingHorizontal: 11,
-    alignSelf: "flex-start",
-  },
-  postLastInfo: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-  },
-  postPrice: {
-    fontSize: 18,
-    color: theme.textDark,
-    fontWeight: "700",
-    paddingTop: 6,
-  },
-  postHeart: {
-    width: 20,
-    height: 18,
-  },
+    post: {
+        flexDirection: "row",
+        paddingHorizontal: 25,
+        paddingTop: 25,
+    },
+    postContent: {
+        paddingLeft: 20,
+    },
+    postImg: {
+        width: 120,
+        height: 120,
+    },
+    postTitle: {
+        fontSize: 20,
+        color: theme.textDark,
+    },
+    postInfo: {
+        flexDirection: "row",
+        paddingTop: 5,
+        paddingBottom: 17,
+    },
+    postInfoText: {
+        fontSize: 15,
+        color: theme.textLight,
+    },
+    postType: {
+        fontSize: 15,
+        color: theme.textDark,
+        borderRadius: 15,
+        backgroundColor: "#EEEEEE",
+        paddingVertical: 3,
+        paddingHorizontal: 11,
+        alignSelf: "flex-start",
+    },
+    postLastInfo: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+    },
+    postPrice: {
+        fontSize: 18,
+        color: theme.textDark,
+        fontWeight: "700",
+        paddingTop: 6,
+    },
+    postHeart: {
+        width: 20,
+        height: 18,
+    },
 });
