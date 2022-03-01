1d9f726076f75e4cff3fc2edd28fb09c9f279bb1
Jimin Kim <0305jimin@gmail.com>
2022-03-01 20:09:19 +0900

    design: navigation bar

---
+++ navigationBar.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import { theme } from "../../colors";
+import React from "react";
+export default function Post({
+    
+}) {
+    return (
+       
+    );
+}
+
+const styles = StyleSheet.create({
+    
+});
